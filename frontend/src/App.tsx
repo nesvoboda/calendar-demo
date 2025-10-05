0b046c8b771f3255bb61288c89870098720f1d78
A S <ashishae@student.42.fr>
2025-10-05 23:32:11 +0200

    Indicate bookable hours

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Calendar } from "./components/calendar";
 function App() {
   return (
     <div className="min-h-svh flex flex-col max-w-4xl w-full mx-auto py-20">
-      <h1 className="text-2xl font-bold">Book your meeting</h1>
       <Calendar />
     </div>
   );
--- frontend/src/components/calendar.tsx
+++ frontend/src/components/calendar.tsx
@@ -1,12 +1,42 @@
 import { useMemo, useState } from "react";
-import { startOfWeek, endOfWeek, eachDayOfInterval, format } from "date-fns";
+import {
+  startOfWeek,
+  endOfWeek,
+  eachDayOfInterval,
+  format,
+  subWeeks,
+  addWeeks,
+  addHours,
+  isAfter,
+} from "date-fns";
+import { Button } from "./ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+import clsx from "clsx";
 
 export function Calendar() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   return (
-    <div className="flex-1 flex flex-row gap-4 h-full">
-      <Timeline />
-      <Week selectedDate={selectedDate} />
+    <div className="flex flex-col h-full flex-1 gap-4">
+      <h1 className="text-2xl font-bold">Book your meeting</h1>
+      <div className="flex flex-row gap-2">
+        <Button
+          variant="outline"
+          onClick={() => setSelectedDate(subWeeks(selectedDate, 1))}
+        >
+          <ChevronLeft /> Previous week
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => setSelectedDate(addWeeks(selectedDate, 1))}
+        >
+          Next week <ChevronRight />
+        </Button>
+      </div>
+      <div className="flex-1 flex flex-row gap-4 h-full">
+        <Timeline />
+        <Week selectedDate={selectedDate} />
+      </div>
     </div>
   );
 }
@@ -57,7 +87,7 @@ export function Day({ date }: { date: Date }) {
 
         <div className="flex flex-col h-full">
           {Array.from({ length: 24 }).map((_, index) => (
-            <Hour key={index} date={new Date(date.setHours(index, 0, 0, 0))} />
+            <Hour key={index} startOfHour={addHours(date, index)} />
           ))}
         </div>
       </div>
@@ -65,8 +95,14 @@ export function Day({ date }: { date: Date }) {
   );
 }
 
-export function Hour({ date }: { date: Date }) {
+export function Hour({ startOfHour }: { startOfHour: Date }) {
+  const bookable = isAfter(startOfHour, new Date());
   return (
-    <div className="flex-1 bg-gray-100 border-b border-r border-gray-200"></div>
+    <div
+      className={clsx(
+        "flex-1 bg-gray-100 border-b border-r border-gray-200",
+        bookable && "bg-green-50"
+      )}
+    ></div>
   );
 }
