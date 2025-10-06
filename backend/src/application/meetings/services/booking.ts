6066e683919d45fee97ed71fac081744ab1c6774
A S <ashishae@student.42.fr>
2025-10-06 10:32:32 +0200

    Add a lock

--- backend/src/application/meetings/services/booking.ts
+++ backend/src/application/meetings/services/booking.ts
@@ -5,34 +5,37 @@ import type {
   MeetingCreate,
 } from "../../../domain/meetings/types";
 import { OverlapsError, type IZoomAPI } from "../interfaces/zoom";
-import {
-  addMinutes,
-  areIntervalsOverlapping,
-  interval,
-  isWithinInterval,
-} from "date-fns";
+import { addMinutes, areIntervalsOverlapping, interval } from "date-fns";
+
+import { Mutex } from "async-mutex";
+
 export class BookingService {
   constructor(private readonly zoomAPI: IZoomAPI) {}
+  private readonly mutex = new Mutex();
 
   async createMeeting(
     meeting: MeetingCreate
   ): Promise<Result<CreatedMeeting, OverlapsError>> {
-    const meetings = await this.listMeetings();
-    const intervals = meetings.map((m) =>
-      interval(m.startDate, addMinutes(m.startDate, m.duration))
-    );
-    const candidateInterval = interval(
-      meeting.startDate,
-      addMinutes(meeting.startDate, meeting.duration)
-    );
-    if (
-      intervals.some((interval) =>
-        areIntervalsOverlapping(candidateInterval, interval)
-      )
-    ) {
-      return err(new OverlapsError("Meeting overlaps with existing meetings"));
-    }
-    return this.zoomAPI.createMeeting(meeting);
+    return await this.mutex.runExclusive(async () => {
+      const meetings = await this.listMeetings();
+      const intervals = meetings.map((m) =>
+        interval(m.startDate, addMinutes(m.startDate, m.duration))
+      );
+      const candidateInterval = interval(
+        meeting.startDate,
+        addMinutes(meeting.startDate, meeting.duration)
+      );
+      if (
+        intervals.some((interval) =>
+          areIntervalsOverlapping(candidateInterval, interval)
+        )
+      ) {
+        return err(
+          new OverlapsError("Meeting overlaps with existing meetings")
+        );
+      }
+      return await this.zoomAPI.createMeeting(meeting);
+    });
   }
 
   async listMeetings(): Promise<Meeting[]> {
